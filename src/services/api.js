1bd03c294cf7046c1f7bcdbf1d019f2028f0d49e
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2024-04-15 00:03:00 -0300

    5

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 // https://api.themoviedb.org/3/movie/now_playing?api_key=362663ab767ca323fcb96f2cb10cd6e7&language=pt-br
 
 import axios from "axios";
-import Axios from "axios";
 
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/'
